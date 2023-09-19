405e1e7e7b16ab895a6b510a58df1c32b8737022
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2023-09-19 10:21:10 +0200

    Creation projet + page accueil

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
 
 export default function App() {
+
+  const PlaceholderImage = require('./assets/images/accueil.jpeg');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Maire de Simplonville</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage}/>
+      </View>      
+      <View style={styles.footerContainer}>
+        <Button label="Formulaire d'alerte" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    color:'white',
+    paddingTop: 120,
+    fontSize: 20
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 50,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
